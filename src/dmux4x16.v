b6064199eb05962bad232eb4f7601eb686be8c69
Barrett <barrettotte@gmail.com>
2020-06-07 20:17:20 -0400

    small change to register file

--- src/dmux4x16.v
+++ src/dmux4x16.v
@@ -1,4 +1,4 @@
-/* 4 to 16 demultiplexer (decoder) */
+/* 4 to 16 demultiplexer (dmux or decoder) */
 
 `timescale 1ns/1ns
 
--- src/regfile.v
+++ src/regfile.v
@@ -8,10 +8,15 @@
 `ifndef _regfile
 `define _regfile
 
-/* example instruction:  
+/* 
+  example instruction:
   ADD R0,R1,R2
-  Ins  Rd   Ra   Rb
-  ???? 0001 0010 0011
+
+  | Ins  | Rd   | Ra   | Rb   |
+  | ---- | ---- | ---- | ---- |
+  | ADD  | R0   | R1   | R2   |
+  | TBD? | 0001 | 0010 | 0011 |
+
 */
 
 module regfile(
@@ -30,26 +35,27 @@ module regfile(
   wire [15:0] enRa; // enable signals for Ra selection
   wire [15:0] enRb; // enable signals for Rb selection
 
-  dmux4x16 d0(wen,  selRd, enRd);
-  dmux4x16 d1(1'b1, selRa, enRa);
-  dmux4x16 d2(1'b1, selRb, enRb);
+  // three 4x16 dmux for selecting registers
+  dmux4x16 d0(.en(wen),  .sel(selRd), .d(enRd));
+  dmux4x16 d1(.en(1'b1), .sel(selRa), .d(enRa));
+  dmux4x16 d2(.en(1'b1), .sel(selRb), .d(enRb));
 
-  reg16 r00(clk, rst, enRd[0],  enRa[0],  enRb[0],  rd, ra, rb);
-  reg16 r01(clk, rst, enRd[1],  enRa[1],  enRb[1],  rd, ra, rb);
-  reg16 r02(clk, rst, enRd[2],  enRa[2],  enRb[2],  rd, ra, rb);
-  reg16 r03(clk, rst, enRd[3],  enRa[3],  enRb[3],  rd, ra, rb);
-  reg16 r04(clk, rst, enRd[4],  enRa[4],  enRb[4],  rd, ra, rb);
-  reg16 r05(clk, rst, enRd[5],  enRa[5],  enRb[5],  rd, ra, rb);
-  reg16 r06(clk, rst, enRd[6],  enRa[6],  enRb[6],  rd, ra, rb);
-  reg16 r07(clk, rst, enRd[7],  enRa[7],  enRb[7],  rd, ra, rb);
-  reg16 r08(clk, rst, enRd[8],  enRa[8],  enRb[8],  rd, ra, rb);
-  reg16 r09(clk, rst, enRd[9],  enRa[9],  enRb[9],  rd, ra, rb);
-  reg16 r10(clk, rst, enRd[10], enRa[10], enRb[10], rd, ra, rb);
-  reg16 r11(clk, rst, enRd[11], enRa[11], enRb[11], rd, ra, rb);
-  reg16 r12(clk, rst, enRd[12], enRa[12], enRb[12], rd, ra, rb);
-  reg16 r13(clk, rst, enRd[13], enRa[13], enRb[13], rd, ra, rb);
-  reg16 r14(clk, rst, enRd[14], enRa[14], enRb[14], rd, ra, rb);
-  reg16 r15(clk, rst, enRd[15], enRa[15], enRb[15], rd, ra, rb);
+  reg16 r00(.clk(clk), .rst(rst), .en(enRd[0]),  .selA(enRa[0]),  .selB(enRb[0]),  .bus(rd), .a(ra), .b(rb));
+  reg16 r01(.clk(clk), .rst(rst), .en(enRd[1]),  .selA(enRa[1]),  .selB(enRb[1]),  .bus(rd), .a(ra), .b(rb));
+  reg16 r02(.clk(clk), .rst(rst), .en(enRd[2]),  .selA(enRa[2]),  .selB(enRb[2]),  .bus(rd), .a(ra), .b(rb));
+  reg16 r03(.clk(clk), .rst(rst), .en(enRd[3]),  .selA(enRa[3]),  .selB(enRb[3]),  .bus(rd), .a(ra), .b(rb));
+  reg16 r04(.clk(clk), .rst(rst), .en(enRd[4]),  .selA(enRa[4]),  .selB(enRb[4]),  .bus(rd), .a(ra), .b(rb));
+  reg16 r05(.clk(clk), .rst(rst), .en(enRd[5]),  .selA(enRa[5]),  .selB(enRb[5]),  .bus(rd), .a(ra), .b(rb));
+  reg16 r06(.clk(clk), .rst(rst), .en(enRd[6]),  .selA(enRa[6]),  .selB(enRb[6]),  .bus(rd), .a(ra), .b(rb));
+  reg16 r07(.clk(clk), .rst(rst), .en(enRd[7]),  .selA(enRa[7]),  .selB(enRb[7]),  .bus(rd), .a(ra), .b(rb));
+  reg16 r08(.clk(clk), .rst(rst), .en(enRd[8]),  .selA(enRa[8]),  .selB(enRb[8]),  .bus(rd), .a(ra), .b(rb));
+  reg16 r09(.clk(clk), .rst(rst), .en(enRd[9]),  .selA(enRa[9]),  .selB(enRb[9]),  .bus(rd), .a(ra), .b(rb));
+  reg16 r10(.clk(clk), .rst(rst), .en(enRd[10]), .selA(enRa[10]), .selB(enRb[10]), .bus(rd), .a(ra), .b(rb));
+  reg16 r11(.clk(clk), .rst(rst), .en(enRd[11]), .selA(enRa[11]), .selB(enRb[11]), .bus(rd), .a(ra), .b(rb));
+  reg16 r12(.clk(clk), .rst(rst), .en(enRd[12]), .selA(enRa[12]), .selB(enRb[12]), .bus(rd), .a(ra), .b(rb));
+  reg16 r13(.clk(clk), .rst(rst), .en(enRd[13]), .selA(enRa[13]), .selB(enRb[13]), .bus(rd), .a(ra), .b(rb));
+  reg16 r14(.clk(clk), .rst(rst), .en(enRd[14]), .selA(enRa[14]), .selB(enRb[14]), .bus(rd), .a(ra), .b(rb));
+  reg16 r15(.clk(clk), .rst(rst), .en(enRd[15]), .selA(enRa[15]), .selB(enRb[15]), .bus(rd), .a(ra), .b(rb));
 
 endmodule
 
