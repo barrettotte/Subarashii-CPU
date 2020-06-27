bc9e62d4cd36bee952437a7cc54716950f8b0406
Barrett <barrettotte@gmail.com>
2020-06-27 00:11:57 -0400

    change specs and progress on ALU

---
+++ src/alu.v
@@ -0,0 +1,39 @@
+/* Arithmetic Logic Unit (ALU) */
+
+`timescale 1ns/1ns
+
+`ifndef _alu
+`define _alu
+
+module alu(
+  input  [15:0] rs, // register Rs
+  input  [15:0] rt, // register Rt
+  input   [2:0] op, // ALU operation
+  output fN,        // negative flag
+  output fZ,        // zero flag
+  output fC,        // carry flag
+  output fP,        // parity flag
+  output [15:0] rd  // register Rd
+);
+
+always @(rs or rt or op) begin
+  case(op)
+    4'b000:  {fC, rd} = rs + rt;                 // ADD
+    4'b001:  {fC, rd} = rs - rt;                 // SUB
+    4'b010:  {fC, rd} = {1'b0, rs & rt};         // AND
+    4'b011:  {fC, rd} = {1'b0, rs | rt};         // ORR
+    4'b100:  {fC, rd} = {1'b0, ~rs};             // NOT
+    4'b101:  {fC, rd} = {1'b0, rs ^ rt};         // XOR
+    4'b110:  begin fC = rs[0]; rd = rs >> 1; end // LSR
+    4'b111:  begin fC = rs[0]; rd = rs << 1; end // LSL
+    default: {fC, rd} = {1'b0, rs};              //
+  endcase
+
+  fN = rd[15];
+  fZ = (rd == 16'b0)   ? 1'b1 : 1'b0; // 
+  fP = (rd[0] == 1'b0) ? 1'b1 : 1'b0; // odd : even
+end
+
+endmodule
+
+`endif
--- src/specs.v
+++ src/specs.v
@@ -2,65 +2,93 @@
 
 
 16x16-bit registers
-  * R0-R11 - general purpose
-  * R12 - scratch register
-  * R13 - status register
-  * R14 - stack pointer
-  * R15 - program counter
+  * R0     - zero register
+  * R1-R11 - general purpose
+  * R12    - scratch register
+  * R13    - status register (flags, control, errors)
+  * R14    - stack pointer
+  * R15    - program counter
+
 
 
 ALU Operations:
-  1000 - ADD
-  1001 - SUB
-  1010 - AND
-  1011 - ORR
-  1100 - NOT
-  1101 - XOR
-  1110 - LSR
-  1111 - LSL
+  000 - ADD
+  001 - SUB
+  010 - AND
+  011 - ORR
+  100 - NOT
+  101 - XOR
+  110 - LSR
+  111 - LSL
 
 
-Instruction Set
-  1.  NOP              ; No operation
-  2.  ADD R0,R1,R2     ; R0 = R1 + R2
-  3.  SUB R0,R1,R2     ; R0 = R1 - R2
-  4.  AND R0,R1,R2     ; R0 = R1 & R2
-  5.  ORR R0,R1,R2     ; R0 = R1 | R2
-  6.  NOT R0,R1        ; R0 = ~R1  
-  7.  XOR R0,R1,R2     ; R0 = R1 ^ R2
-  8.  LSR R0,R1        ; R0 = R1 >> 1
-  9.  LSL R0,R1        ; R0 = R1 << 1
-  
-  10. LDL R0,00001111  ; R0 = ????????00001111 (load immediate into lower byte of register)
-  11. LDU R0,11110000  ; R0 = 11110000???????? (load immediate into upper byte of register)
-  12. LDW R0,R1,R2     ; R0 = RAM[R1] + offset  (load word to register from memory address)
-  13. STW R0,R1,R2     ; RAM[R0] + offset = R1  (store word at memory address)
-  
-  14. BEQ R0           ; Set PC to value in R0 if Z flag = 0
+Status Register:
+| Index | Symbol | Name             | Description                       |
+| ----- | ------ | ---------------- | --------------------------------- |
+| 0     | Z      | Zero             | Result of operation was zero      |
+| 1     | C      | Carry            |                                   |
+| 2     | N      | Negative         | Result of operation was negative  |
+| 3     | V      | Overflow         | Result of operation was too large |
+| 4     | P      | Parity           | Even=1, Odd=0                     |
+| 5     | D      | Decimal          | Flag useful for BCD               |
+| 6     | I      | Interrupt Enable |                                   |
+| 7     |        |                  |                                   |
+| 8     |        |                  |                                   |
+| 9     |        |                  |                                   |
+| 10    |        |                  |                                   |
+| 11    |        |                  |                                   |
+| 12    |        |                  |                                   |
+| 13    |        |                  |                                   |
+| 14    |        |                  |                                   |
+| 15    |        |                  |                                   |
 
-  15. PSH R0
-  16. POP R0
+
+
+| OPCODE | SYNTAX          | LOGIC                 | DESCRIPTION                            |
+| ------ | --------------- | --------------------- | -------------------------------------- |
+| 0000   | ADD R0,R1,R2    | R0 = R1 + R2          | register addition                      |
+| 0001   | SUB R0,R1,R2    | R0 = R1 - R2          | register subtraction                   |
+| 0010   | AND R0,R1,R2    | R0 = R1 & R2          | logical AND                            |
+| 0011   | ORR R0,R1,R2    | R0 = R1 | R2          | logical OR                             |
+| 0100   | NOT R0,R1       | R0 = ~R1              | logical NOT                            |
+| 0101   | XOR R0,R1,R2    | R0 = R1 ^ R2          | logical XOR                            |
+| 0110   | LSR R0,R1       | R0 = R1 >> 1          | logical shift right                    |
+| 0111   | LSL R0,R1       | R0 = R1 << 1          | logical shift left                     |
+| 1000   | LDL R0,00001111 | R0 = ????????00001111 | load immediate lower                   |
+| 1001   | LDU R0,00001111 | R0 = 00001111???????? | load immediate upper                   |
+| 1010   | LDW R0,R1,R2    | R0 = [R1+R2]          | load word from memory address + offset |
+| 1011   | STW R0,R1,R2    | [R0+R2] = R1          | store word at memory address + offset  |
+| 1100   | BEQ R0          | PC = Z ? R0 : PC      | branch if equal                        |
+| 1101   |
+| 1110   |
+| 1111   |
+
+
 
 
 Macro Instructions:
-  1. INC R0,0x01       
+  1. INC R1,0x01       
     * LDI R12,0x01     ; load scratch register with increment
-    * ADD R0,R0,R12    ; increment R0 by R12
+    * ADD R1,R1,R12    ; increment R0 by R12
   2. DEC R0,0x01
     * LDI R12,0x01     ; load scratch register with decrement
     * SUB R0,R0,R12    ; decrement R0 by R12
   3. PSH R0
-    * LDI R12,0x02     ; load scratch register with increment
+    * XOR R12,R12,R12  ; clear scratch register
+    * LDL R12,0x02     ; load scratch register lower with increment
     * ADD R14,R14,R12  ; increment stack pointer
     * STW R14,R0       ; store R0 in stack memory
-  4. POP R0
-    * LDI R12,0x02     ; load scratch register with decrement
+  4. POP R1
+    * XOR R12,R12,R12  ; clear scratch register
+    * LDL R12,0x02     ; load scratch register lower with decrement
     * SUB R14,R14,R12  ; decrement stack pointer
-    * LDW R0,R14       ; load register from stack
-  5. CMP R0,R1
-    * SUB R12,R0,R1    ; sets flags, uses scratch register
-  6. CLR R0
-    * XOR R0,R0,R0     ; clear register contents
+    * LDW R1,R14       ; load register from stack
+  5. CMP R1,R2
+    * SUB R12,R1,R2    ; sets flags, uses scratch register
+  6. CLR R1
+    * XOR R1,R1,R1     ; clear register contents
+  7. NOP               ; no operation
+    * ADD R0,R0,R0     ;
 
 
 Instruction Notes
